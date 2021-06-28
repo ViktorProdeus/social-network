9a89bd90a0269935a1ee801c9220d69b52d9f19e
ViktorProdeus <viktorprodeus@gmail.com>
2021-06-28 17:26:01 +0300

    fix BrowserRouter to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 let rerenderEntireTree = ((state: StateType) => {
     ReactDOM.render(
         <React.StrictMode>
-            <BrowserRouter>
+            <HashRouter>
                 <App
                     state={state}
                     dispatch={store.dispatch.bind(store)}
                />
-            </BrowserRouter>
+            </HashRouter>
         </React.StrictMode>,
         document.getElementById('root')
     );
